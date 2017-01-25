e7dc3636c59d21a48d023498ab69daa2ae30097c
XIIFulminata <jovaunjackson@gmail.com>
2017-01-24 22:21:56 -0500

    add slick slider to instron page

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.clamp-slider').slick({
+      dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+      arrows: true,
+  });
+});

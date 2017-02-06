1c3a955c1faf1999d3c2b8a8d94ed5a33b082709
XIIFulminata <jovaunjackson@gmail.com>
2017-02-05 23:21:20 -0500

    added instron assets

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 
-$(document).ready(function(){
+/*$(document).ready(function(){
   $('.clamp-slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
       arrows: true,
   });
-});
+});*/
 
 $( ".instron-link" ).click(function() {
   location.href="instron.html"
@@ -21,3 +21,13 @@ $( ".GE-link" ).click(function() {
 /*$( ".REU-link" ).click(function() {
   location.href="experience.html#ge-exp-card"
 });*/
+/*
+$( ".raw-link" ).click(function() {
+    window.open('http://rawtours.org/');
+});
+
+
+$( ".bess-link" ).click(function() {
+    window.open('http://nuweb2.neu.edu/bess/');
+});
+*/
